b5d2c945a1fec3719f0f90b37da471c66d8b3bed
Allan Benelli <allan.benelli@bluewin.ch>
2021-11-04 18:10:12 +0100

    Added antonioribeiro tracker

--- target-service/app/Http/Kernel.php
+++ target-service/app/Http/Kernel.php
@@ -21,6 +21,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \PragmaRX\Tracker\Vendor\Laravel\Middlewares\Tracker::class,
+
     ];
 
     /**
@@ -37,6 +39,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \PragmaRX\Tracker\Vendor\Laravel\Middlewares\Tracker::class,
+
         ],
 
         'api' => [
--- target-service/config/app.php
+++ target-service/config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        PragmaRX\Tracker\Vendor\Laravel\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Tracker' => 'PragmaRX\Tracker\Vendor\Laravel\Facade',
 
     ],
 

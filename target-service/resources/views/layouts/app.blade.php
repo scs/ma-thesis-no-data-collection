d4a80374fb1f404e6f7143ba6c30b77c2847eb48
Allan Benelli <allan.benelli@bluewin.ch>
2021-11-05 13:13:27 +0100

    Add Again Haruncpi

--- target-service/resources/views/layouts/app.blade.php
+++ target-service/resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('tracker.stats.index') }}">{{ __('Stats') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin/user-activity">{{ __('User-Activity') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->

0ca886f5ac77e81de26bb5ca9c4d9a43cd6d7848
Allan Benelli <allan.benelli@bluewin.ch>
2021-11-05 11:01:38 +0100

    Add small navigation to stats

--- target-service/resources/views/layouts/app.blade.php
+++ target-service/resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('tracker.stats.index') }}">{{ __('Stats') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
